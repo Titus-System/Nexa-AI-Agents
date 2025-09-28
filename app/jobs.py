896ab66b5f76a62d1620d8b01e6ee41c1ac9e1b9
Pedro Garcia <pedrofsgarcia.pro@gmail.com>
2025-09-28 19:21:24 -0300

    fix(schemas): corrige schemas para garantir que job_id é opcional - corrige imports e caminhos

--- app/jobs.py
+++ app/jobs.py
@@ -14,29 +14,34 @@ def start_single_classification_job(data: SingleClassificationRequest, job_id: s
         f"Iniciando job para partnumber {data.partnumber} no canal {data.progress_channel}"
     )
     try:
-        for i in range(3):
-            time.sleep(1)
-            progress_schema = ProgressSchema(
-                current=i + 1, total=3, message="Analisando dados no servidor Nexa-IA"
-            )
-            redis_publisher.send_progress_update(data.progress_channel, progress_schema)
+        # for i in range(3):
+        #     time.sleep(1)
+        #     progress_schema = ProgressSchema(
+        #         current=i + 1, total=3, message="Analisando dados no servidor Nexa-IA"
+        #     )
+        #     redis_publisher.send_progress_update(data.progress_channel, progress_schema)
 
-            # call manager agent
-            manager_agent(channel=data.progress_channel, job_id=job_id, data=data)
+        # call manager agent
 
-        time.sleep(1)
-        result = SingleClassification(
-            partnumber=data.partnumber,
-            ncm="123456788",
-            description="Descrição detalhada do produto",
-            exception="01",
-            nve="01",
-            fabricante="fábrica Nexa",
-            endereco="av pequim",
-            pais="China",
-            confidence_score=0.98,
+        progress_schema = ProgressSchema(
+            current=1, total=1, message="Analisando dados no servidor Nexa-IA"
         )
-        redis_publisher.send_done_classification(data.progress_channel, result)
+        # redis_publisher.send_progress_update(data.progress_channel, progress_schema)
+        manager_agent(channel=data.progress_channel, job_id=job_id, data=data)
+
+        # time.sleep(1)
+        # result = SingleClassification(
+        #     partnumber=data.partnumber,
+        #     ncm="123456788",
+        #     description="Descrição detalhada do produto",
+        #     exception="01",
+        #     nve="01",
+        #     fabricante="fábrica Nexa",
+        #     endereco="av pequim",
+        #     pais="China",
+        #     confidence_score=0.98,
+        # )
+        # redis_publisher.send_done_classification(data.progress_channel, result)
         return
 
     except Exception as e:
